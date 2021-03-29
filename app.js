6403fb49c62e6fc618cb02a4c754f0c09704ee5e
daniilpolienko <dan.polienko.dev@gmail.com>
2021-03-29 17:12:31 +0300

    Add .env to .gitignore

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use('/item', require('./controllers/item.post'))
 app.use('/item', require('./controllers/item.delete'))
 app.use('/item', require('./controllers/item.patch'))
 
-app.listen(process.env.PORT || 5003, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
