3a439f77ed55300d3a7c2222ed12f2a0e7baef00
daniilpolienko <dan.polienko.dev@gmail.com>
2021-03-29 16:26:49 +0300

    heroku deplotwer

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use('/item', require('./controllers/item.post'))
 app.use('/item', require('./controllers/item.delete'))
 app.use('/item', require('./controllers/item.patch'))
 
-app.listen(process.env.PORT || 3008, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
