b9b3e7c691ca4f83c8770c1d7dbe64c11c16b9be
daniilpolienko <dan.polienko.dev@gmail.com>
2021-03-29 16:41:06 +0300

    herooooku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3008
 
 
 app.use(bodyParser.json())
@@ -12,6 +12,6 @@ app.use('/item', require('./controllers/item.post'))
 app.use('/item', require('./controllers/item.delete'))
 app.use('/item', require('./controllers/item.patch'))
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || "5001", () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
