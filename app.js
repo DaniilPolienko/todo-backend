a3d52acea56e0a82a19e24958023e6f20d9cbc8f
daniilpolienko <dan.polienko.dev@gmail.com>
2021-03-30 11:08:05 +0300

    files creates if didnt exist before

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 // const dotenv = require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 const app = express()
-
+const path = './tasks.json'
+const emptyArray = []
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
+if (fs.existsSync(path)) console.log('The path exists.');
+  else {
+    fs.open(path, 'w', (err) => {
+      if(err) throw err;
+      console.log('File created');
+    });
+  fs.writeFileSync(path, JSON.stringify(emptyArray))
+  }
 
 app.use('/items', require('./controllers/items.get'))
 app.use('/item', require('./controllers/item.post'))
