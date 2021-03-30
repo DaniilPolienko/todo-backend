402e473e045bf234aaa626ebff7f92dff50f1e6e
daniilpolienko <dan.polienko.dev@gmail.com>
2021-03-30 16:06:49 +0300

    updated get

--- controllers/items.get.js
+++ controllers/items.get.js
@@ -9,25 +9,27 @@ const Router = e.Router()
 const get = Router.get('/', (req, res) => {
     let tasks  = fs.readFileSync(path, 'utf8')
     let newItems = []
+    const json = JSON.parse(tasks)  
+    console.log(req.query.params)
     switch (req.query.sort) {
       case 'asc':
-        newItems = tasks
+        newItems = json
         break;
       case 'desc':
-      newItems = tasks.reverse()
+      newItems = json.reverse()
         break;
       default: 
-        newItems = tasks
+        newItems = json
     }
     switch (req.query.filter) {
       case 'done':
-        newItems = tasks.filter(item => item.done == true)
+        newItems = json.filter(item => item.done == true)
         break;
       case 'undone':
-        newItems = tasks.filter(item=>item.done == false)
+        newItems = json.filter(item=>item.done == false)
         break;
       default:
-        newItems = tasks
+        newItems = json
         break;
     }
     res.send(newItems)
