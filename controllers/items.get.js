7a584c4c98bce911ffedd703657253b760ebd6a5
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-02 16:06:38 +0300

    Add get query validatin

--- controllers/items.get.js
+++ controllers/items.get.js
@@ -1,25 +1,35 @@
 const e = require("express");
 const Router = e.Router();
 const { Task } = require("../models");
+const { query, validationResult } = require("express-validator");
 //query validation
-const get = Router.get("/", async (req, res) => {
-  try {
-    const filter = {
-      where: {},
-      order: [],
-    };
-    if (req.query.filter) filter.where = { done: req.query.filter };
-    if (req.query.sort)
-      filter.order.push([
-        "createdAt",
-        req.query.sort === "asc" ? "ASC" : "DESC",
-      ]);
+const get = Router.get(
+  "/",
+  query("filter").optional().isBoolean(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    const items = await Task.findAll(filter);
+      const filter = {
+        where: {},
+        order: [],
+      };
+      if (req.query.filter) filter.where = { done: req.query.filter };
+      if (req.query.sort)
+        filter.order.push([
+          "createdAt",
+          req.query.sort === "asc" ? "ASC" : "DESC",
+        ]);
 
-    res.send(items);
-  } catch (err) {
-    return res.status(400).send(err.message);
+      const items = await Task.findAll(filter);
+
+      res.send(items);
+    } catch (err) {
+      return res.status(400).send(err.message);
+    }
   }
-});
+);
 module.exports = get;
