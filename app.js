39b115a7cbcb9597e1c777afa53141f46d7833d3
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-05 11:29:03 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 
 const db = require("./models");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use("/items", require("./controllers/items.get"));
 app.use("/item", require("./controllers/item.post"));
 app.use("/item", require("./controllers/item.delete"));
