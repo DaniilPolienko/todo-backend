6c663311c95ddebdc8b2ca8bc32ccdd6903e852d
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-15 11:29:00 +0300

    Set up sequelize env to work with the local database

---
+++ config/config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+module.exports = {
+  [process.env.NODE_ENV]: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
